4adee8e7b37df6aaf2618d5f84c683b9059aded6
Aaron Ureel <aaron.ureel@gmail.com>
2014-08-16 22:51:20 -0400

    Refatored functional tests to follow DRY principle

--- functional_tests.py
+++ functional_tests.py
@@ -10,6 +10,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Kelly has heard about a cool new online to-do app.
         # She goes to check out its homepage
@@ -30,10 +35,7 @@ class NewVisitorTest(unittest.TestCase):
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy shoes from DSW" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy shoes from DSW', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy shoes from DSW')
 
         # There is still a text box inviting her to add another item. 
         # She enters "Pack new shoes for Vegas trip"
@@ -42,10 +44,8 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
 
         # The page updates again, and now shows both items on her list
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy shoes from DSW', [row.text for row in rows])
-        self.assertIn('2: Pack new shoes for Vegas trip', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy shoes from DSW')
+        self.check_for_row_in_list_table('2: Pack new shoes for Vegas trip')
 
         # Kelly wonders whether the site will remember her list.
         # Then she sees that the site has generated a unique URL for her
